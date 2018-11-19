edc69fa6928acb66873906ad1757ef1517903a77
Hans Li <lijunhao.aug@gmail.com>
2018-11-18 23:03:33 -0800

    S3 end, set up course project complete

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe (
+      'A Test Recipe',
+      'A Test',
+      'https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large2x.jpg'),
+      new Recipe (
+        'A Test Recipe 2',
+        'A Test',
+        'https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large2x.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
