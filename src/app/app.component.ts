680e78d9edffd0195e2ab5f8f74599e8b93b4176
Hans Li <lijunhao.aug@gmail.com>
2018-12-25 22:50:14 -0800

    s20 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC-AvpfVMYfqd5VLYR8PCkIAIHQp2SSuLE",
+      authDomain: "angularlearning-hans.firebaseapp.com"
+    });
+  }
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,19 +4,24 @@ import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 
 import 'rxjs/Rx';
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
     constructor(
         private http: Http,
-        private recipeService: RecipeService) {}
+        private recipeService: RecipeService,
+        private authService: AuthService) {}
 
     storeRecipe() {
-        return this.http.put('https://angularlearning-hans.firebaseio.com/recipe.json', this.recipeService.getRecipes());
+        let tk = this.authService.getToken();
+        return this.http.put('https://angularlearning-hans.firebaseio.com/recipe.json?auth=' + tk, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        return this.http.get('https://angularlearning-hans.firebaseio.com/recipe.json')
+        let tk = this.authService.getToken();
+
+        return this.http.get('https://angularlearning-hans.firebaseio.com/recipe.json?auth=' + tk)
         .map((response: Response) => {
             const recipes = response.json();
             for (let recipe of recipes) {
