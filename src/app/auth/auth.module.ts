28de867580b9a6ef6f7542161248cd0e9a58bfec
Hans Li <hans@Hanss-MacBook-Pro.local>
2018-12-26 16:00:22 -0800

    separate feature into module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { SignupComponent } from "./signup/signup.component";
+import { SigninComponent } from "./signin/signin.component";
+
+@NgModule({
+    declarations: [
+        SignupComponent,
+        SigninComponent
+    ],
+    imports: [FormsModule]
+})
+export class AuthModule {}
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
+
+import { RecipesComponent } from "./recipes.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeStartComponent,
+        RecipeListComponent,
+        RecipeEditComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule,
+        SharedModule
+    ],
+    exports: []
+})
+export class RecipesModule {}
